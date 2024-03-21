7280d78edfcf1fcf8df7c2597bdf14352917d1e5
Samkayani3 <kayaniusama532@gmail.com>
2024-03-21 11:21:16 +0500

    Reset Password Button

--- resources/views/emails/reset-email-link.blade.php
+++ resources/views/emails/reset-email-link.blade.php
@@ -1,10 +1,35 @@
 <!doctype html>
 <html lang="en">
-<body>
-    <h1>Password Reset</h1>
+<style>
+    button{
+        padding: 10px 30px;
+        background-color: #c80062;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
+<!-- <body>
+    <h3>Password Reset</h3>
     <p>Email: {{ $display_email }}</p>
     <p>Reset URL: <a href="{{ $reset_url }}">Click Here</a></p>
     <p>Thank you!</p>
 
-</body>
+</body> -->
+<h2>Password Reset</h2>
+    <p>Email: <span id="display_email"></span></p>
+    <button id="reset_button">Reset Password</button>
+    <p>Thank you!</p>
+
+    <script>
+        var display_email = "{{ $display_email }}";
+        var reset_url = "{{ $reset_url }}";
+
+        document.getElementById("display_email").innerText = display_email;
+        document.getElementById("reset_button").addEventListener("click", function() {
+
+            window.location.href = reset_url;
+        });
+    </script>
 </html>
